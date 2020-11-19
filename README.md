84b7be33610cde39cbd06200760607a0ea610b52
ippee <ippee.music@gmail.com>
2020-11-19 12:46:29 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# otor_vst
+[![Build Status](https://travis-ci.org/ippee/otor_vst.svg?branch=master)](https://travis-ci.org/ippee/otor_vst)
+[![License](https://img.shields.io/badge/license-Apache--2.0_or_MIT-blue)](https://github.com/ippee/otor_vst/blob/master/LICENSE_Apache)  
+
+The prototype of OTOR, which is Otoware Meister, are published below.  
+[https://github.com/ippee/otoware_meister_vst](https://github.com/ippee/otoware_meister_vst)  
+
+## What Is OTOR?
+**OTOR** is a distortion VST plugin which specializes in generating earrape sounds.  
+The name of OTOR comes from the Japanese word "otoware (音割れ)", which means "earrape" in English.  
+
+Because a recent DAW audio engine processes a signal by 64 bit floating point, it is never clipped before it is sent to the D/A converter. However, that means that it is difficult to generate earrape sounds in DAW.  
+
+Thus, to solve this problem, I developed OTOR.  
+
+OTOR can amplify a signal up to 150 dB, but this plugin chops the amplified signal at 0 dBFS forcibly. By doing this, OTOR gives you fierce distorted sounds.  
+
+## Progress
+The basic function of OTOR is already completed, so now I'm developing GUI.  
+If it's done also, I will publish OTOR formally.  
+
+At the moment, OTOR supports **Windows OS only**.  
+
+I want to develope the Mac OS version, but its outlook does not stand at all. If I should receive many requests for this, I would plan something like a crowdfunding campaign.  
+
+## WARNING!
+**EARRAPE SOUNDS MAY DAMAGE YOUR EARS AS WELL AS YOUR AUDIO EQUIPMENT.**  
+**PLEASE NOTE THAT I DO NOT TAKE ANY RESPONSIBILITY OR LIABILITY FOR ANY DAMAGE OR LOSS CAUSED THROUGH THIS PLUGIN.**  
+
+## License
+Licensed under either of
+
+- Apache License, Version 2.0, ([LICENSE_Apache](https://github.com/ippee/otoware_meister_vst/blob/master/LICENSE_Apache) or http://www.apache.org/licenses/LICENSE-2.0)
+- MIT license ([LICENSE_MIT](https://github.com/ippee/otoware_meister_vst/blob/master/LICENSE_MIT) or http://opensource.org/licenses/MIT)
+
+at your option.  
+
+### Contribution
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.  
+
+## Using Sources
+### Programming Language
+- [Rust](https://github.com/rust-lang/rust) (1.42.0)
+
+### Crates
+- [vst-rs](https://github.com/RustAudio/vst-rs) (0.2.0)
+- [rust-vst-gui](https://github.com/ippee/rust-vst-gui.git)
+  - This is a fork of [mikesoylu / rust-vst-gui](https://github.com/mikesoylu/rust-vst-gui)
+  - The origin of them is [vanderlokken / rust-vst-gui](https://github.com/vanderlokken/rust-vst-gui)
+- [lazy_static](https://github.com/rust-lang-nursery/lazy-static.rs) (1.4.0)
---
+++ src/lib.rs
@@ -0,0 +1,139 @@
+#[macro_use]
+extern crate vst;
+extern crate vst_gui;
+
+extern crate lazy_static;
+
+use std::fs::read_to_string;
+use std::sync::{Arc, Mutex};
+use vst::buffer::AudioBuffer;
+use vst::editor::Editor;
+use vst::plugin::{Category, Plugin, Info};
+use lazy_static::lazy_static;
+
+// Convert a decibel value to a linear value.
+fn decibel_to_linear(db: f32) -> f32 {
+    return 10.0_f32.powf(db / 20.0);
+}
+
+// Get the text of index.html.
+// You can build a design from the theme folder. 
+// If you publish a vst plugin using this crete, you should put the
+// theme folder on the directory in the same of the plugin.
+fn load_html() -> String {
+    let content = read_to_string("./theme/index.html");
+    let string_content = match content {
+        Ok(v) => v,
+        Err(_e) => "".to_string(),
+    };
+    string_content
+}
+
+// Load the "index.html"
+lazy_static! {
+    static ref HTML: String = load_html();
+}
+
+struct OtorParameters {
+    pub gain_rate: f32,
+    pub max_gain_level: f32,
+}
+
+fn create_javascript_callback(
+    otor_parameters: Arc<Mutex<OtorParameters>>) -> vst_gui::JavascriptCallback
+{
+    Box::new(move |message: String| {
+        let mut tokens = message.split_whitespace();
+
+        let command = tokens.next().unwrap_or("");
+        let argument = tokens.next().unwrap_or("").parse::<f32>();
+
+        let mut locked_params = otor_parameters.lock().unwrap();
+
+        match command {
+            "getGainRate" => {
+                return locked_params.gain_rate.to_string();
+            },
+            "setGainRate" => {
+                if argument.is_ok() {
+                    locked_params.gain_rate = argument.unwrap();
+                }
+            },
+            _ => {}
+        }
+
+        String::new()
+    })
+}
+
+struct Otor {
+    // We access this object both from a UI thread and from an audio processing
+    // thread.
+    otor_parameters: Arc<Mutex<OtorParameters>>,
+}
+
+impl Default for Otor {
+    fn default() -> Otor {
+        let otor_parameters = Arc::new(Mutex::new(
+            OtorParameters {
+                gain_rate: 0.0, // range: 0.0 to 1.0
+                max_gain_level: 150.0, //dBFS
+            }
+        ));
+
+        Otor {
+            otor_parameters: otor_parameters.clone(),
+        }
+    }
+}
+
+impl Plugin for Otor {
+    fn get_info(&self) -> Info {
+        Info {
+            name: "OTOR".to_string(),
+            vendor: "Ippee".to_string(),
+            unique_id: 48205622,
+            version: 1,
+            inputs: 2,
+            outputs: 2,
+            parameters: 1,
+            category: Category::Effect,
+            ..Default::default()
+        }
+    }
+
+    // Here is where the bulk of our audio processing code goes.
+    fn process(&mut self, buffer: &mut AudioBuffer<f32>) {
+        let locked_params = self.otor_parameters.lock().unwrap();
+
+        let gain_rate = locked_params.gain_rate;
+        let max_gain_level = locked_params.max_gain_level;
+
+        // Destructure an audio buffer into input and output buffers.
+        for (input_buffer, output_buffer) in buffer.zip() {
+
+            for (input_sample, output_sample) in input_buffer.iter().zip(output_buffer) {
+                // Amplify a signal.
+                *output_sample = *input_sample * (decibel_to_linear(max_gain_level * gain_rate));
+
+                // Clip a signal by force.
+                if *output_sample > 1.0 {
+                    *output_sample = 1.0;
+                }
+                else if *output_sample < -1.0 {
+                    *output_sample = -1.0
+                }
+            }
+        }
+    }
+
+    fn get_editor(&mut self) -> Option<Box<dyn Editor>> {
+        let gui = vst_gui::new_plugin_gui(
+            HTML.to_string(),
+            create_javascript_callback(self.otor_parameters.clone()),
+            Some((480, 320)));
+        Some(Box::new(gui))
+    }
+}
+
+plugin_main!(Otor);
